8bd2276ae3f758459f076a76dbd49ee481daf58b
sylphyrose <sylphyrose@gmail.com>
2025-03-25 12:22:10 -0300

    Counted the number of words in frankenstein book

---
+++ main.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+def get_book_text(path_to_file):
+    
+    file_contents = ""
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+def word_count(text):
+    words = []
+    words = text.split()
+    counter = len(words)
+    return counter
+
+def main():
+
+    text = ""
+    count = 0
+    path = Path("books/frankenstein.txt")
+    #print(get_book_text(path))
+    text = get_book_text(path)
+    count = word_count(text)
+    print(f"{count} words found in the document")
+
+main ()
